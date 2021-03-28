195f3ef6ae09814cb914306fc1fcb84f02894eb0
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-03-28 12:34:51 +0600

    basic mongodb

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
+// First Steps
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Update, Thank You For Prot Caling me')
-  })
+// Cors
+const cors = require('cors')
+app.use(cors())
 
-app.listen(3000, () => console.log('listenting to Prot 3000'))
+// Body Perser
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// Second Steps
+const friends = ['sabbir', 'rofik', 'salam', 'jabar', 'kabir', 'achad']
+app.get('/banana', (req, res) =>{
+  res.send({fruit:'banana', quantity: 1200, price:234 })
+})
+
+// Daynamic API
+app.get('/friend/:id', (req, res) =>{
+  const friendId = req.params.id
+  const name = friends[friendId]
+  res.send({friendId, name})
+})
+
+// Post man
+app.post('/addFriends', (req, res) =>{
+  // Save to Database
+  const friend = req.body;
+  friend.id = 34;
+  res.send(friend)
+  console.log('hello post', req.body);
+})
+
+// Thired Steps
+app.listen(4000, () => {
+  console.log(`listening Visit Live Website: http://localhost:${4000}`)
+})
